4b1a2275240766534bdd1ec1b94df5a7c498855b
adriElias <34091624+adriElias@users.noreply.github.com>
2026-04-15 09:52:08 +0200

    feat: add TerrestrialRobot class

---
+++ src/main/java/org/robots/TerrestrialRobot.java
@@ -0,0 +1,40 @@
+package org.robots;
+
+public class TerrestrialRobot extends Robot {
+    public int speedMaxKmH;
+    public TractionType tractionType;
+
+    public TerrestrialRobot(String name, String maker, int creationDate, int speedMaxKmH, TractionType tractionType) {
+        super(name, maker, creationDate, null);
+        this.speedMaxKmH = speedMaxKmH;
+        this.tractionType = tractionType;
+    }
+
+    public int getSpeedMaxKmH() {
+        return speedMaxKmH;
+    }
+
+    public void setSpeedMaxKmH(int speedMaxKmH) {
+        this.speedMaxKmH = speedMaxKmH;
+    }
+
+    public TractionType getTractionType() {
+        return tractionType;
+    }
+
+    public void setTractionType(TractionType tractionType) {
+        this.tractionType = tractionType;
+    }
+
+    @Override
+    public String toString() {
+        return "TerrestrialRobot{" +
+                "speedMaxKmH=" + speedMaxKmH +
+                ", tractionType=" + tractionType +
+                ", name='" + name + '\'' +
+                ", maker='" + maker + '\'' +
+                ", creationDate=" + creationDate +
+                ", dateRegistered=" + dateRegistered +
+                '}';
+    }
+}
